4fad21c5d9dffb2d09509331a8dd1bdd4d4caada
Ezz-Aldyn Husen <34347098+SHADOWZXCV@users.noreply.github.com>
2022-01-27 15:26:57 +0200

    Added script removal and refactored code
    Made a plugin for componentWillUnmount function, just to remove the script to make sure of a correct reload after the component is revisited.

--- pwa/packages/bluecheckageverify-spwa/src/plugin/route/Checkout/CheckoutContainer.plugin.js
+++ pwa/packages/bluecheckageverify-spwa/src/plugin/route/Checkout/CheckoutContainer.plugin.js
@@ -9,26 +9,55 @@
  * @link https://github.com/scandipwa/base-theme
  */
 
-import { blueCheckScriptUrl } from './CheckoutPlugin.config'
-import blueCheckScriptEnabler from '../../../static/blueCheckEnablerScript'
 import { isSignedIn } from 'Util/Auth';
 import BrowserDatabase from 'Util/BrowserDatabase';
 
+import blueCheckScriptEnabler from '../../../static/blueCheckEnablerScript';
+import { blueCheckScriptUrl } from './CheckoutPlugin.config';
 
-export const componentDidMount = (args, callback, instance) => {
-    const isCustomerSignedIn = isSignedIn();
-    const customer = BrowserDatabase.getItem('customer') || {};
-    const blueCheckScript = document.createElement("script");
-    blueCheckScript.onload = () => (blueCheckScriptEnabler(isCustomerSignedIn, customer.addresses, customer.email));
+export const loadScript = () => {
+    const blueCheckScript = document.createElement('script');
+    blueCheckScript.setAttribute('id', 'bluecheck-script');
     blueCheckScript.src = blueCheckScriptUrl;
     document.body.appendChild(blueCheckScript);
+    return new Promise((res, rej) => {
+        blueCheckScript.onload = () => {
+            res();
+        };
+        blueCheckScript.onerror = () => {
+            rej();
+        };
+    });
+};
+
+export const configBlueCheck = () => {
+    const isCustomerSignedIn = isSignedIn();
+    const customer = BrowserDatabase.getItem('customer') || {};
+    return blueCheckScriptEnabler(isCustomerSignedIn, customer.addresses, customer.email);
+};
+
+export const componentDidMount = (args, callback, _instance) => {
+    loadScript().then(() => {
+        configBlueCheck();
+    });
+
+    return callback(...args);
+};
+
+// Removing the script to make sure it is correctly reloaded when revisiting the component again!
+export const componentWillUnmount = (args, callback, _instance) => {
+    window.BlueCheck = null;
+    document.getElementById('bluecheck-container').remove();
+    document.getElementById('bluecheck-script-container').remove();
+    document.getElementById('bluecheck-script').remove();
     return callback(...args);
 };
 
 export default {
     'Route/Checkout/Container': {
         'member-function': {
-            componentDidMount
+            componentDidMount,
+            componentWillUnmount
         }
     }
 };
