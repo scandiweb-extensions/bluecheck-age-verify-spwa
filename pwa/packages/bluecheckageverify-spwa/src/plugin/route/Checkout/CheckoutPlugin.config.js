56776610a8b8de1ab17fcf2162a1d100b7e1d926
Ezz-Aldyn Husen <34347098+SHADOWZXCV@users.noreply.github.com>
2022-02-03 15:06:10 +0200

    Made a global domain token for easier change

--- pwa/packages/bluecheckageverify-spwa/src/plugin/route/Checkout/CheckoutPlugin.config.js
+++ pwa/packages/bluecheckageverify-spwa/src/plugin/route/Checkout/CheckoutPlugin.config.js
@@ -1,1 +1,1 @@
-export const blueCheckScriptUrl = 'https://verify.bluecheck.me/platforms/customintegration/js/AgeVerification-V2.js?domain_token=YDrq7hvGENHOwrzufdXQ';
+export const blueCheckScriptUrl = 'https://verify.bluecheck.me/platforms/customintegration/js/AgeVerification-V2.js?domain_token=<ADD_DOMAIN_TOKEN_HERE>';
